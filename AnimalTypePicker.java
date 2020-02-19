d4e375fb6257dc98362ff76828f95cf4c75c0255
mwaitzman <51432220+mwaitzman@users.noreply.github.com>
2020-02-19 08:54:55 -0500

    initial published version
    the initial version of AnimalTypePicker.java published to GitHub

---
+++ AnimalTypePicker.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class AnimalTypePicker {
+  static Scanner UserInputScanner = new Scanner(System.in);
+  public static void main(String[] args) {
+    BASE_Animals Animals = new BASE_Animals();
+    System.out.println("choose an animal from the following list (type the number to the right of the animal you would like to select):");
+    for (int i=0; i<Animals.TypesOfAnimals.length; i++) {
+      System.out.println("#" + (i+1) + ": " + Animals.TypesOfAnimals[i]);
+      //------------
+    }
+    boolean hasSelected = false;
+    String ChosenAnimal = null;//is null a reserved keyword?? should check sometime
+    do {
+      ChosenAnimal = UserInputScanner.nextLine();
+      try {
+        int ChosenAnimalByNumber = Integer.parseInt(ChosenAnimal);
+        boolean AnimalWasChosenByItsNumber = true;
+      }
+      catch (NumberFormatException ChosenAnimal_NFE) {
+        boolean AnimalWasChosenByItsNumber = false;
+      }
+      try {
+        if (AnimalWasChosenByItsNumber == false) {
+          for(int i=0; i<Animals.TypesOfAnimals.length; i++) {
+            if (ChosenAnimal.equalsIgnoreCase(Animals.TypesOfAnimals[i])) {
+              ChosenAnimal = Animals.TypesOfAnimals[i];
+              hasSelected = true;
+              break;/////////////////////////////////////////////////
+            }
+          }
+        }
+      }
+      catch (Exception AnimalIdentificationLoop) {
+        System.out.println("Unfortunately, an error of type \"" + Exception + "\" has occurred in location \"" + AnimalIdentificationLoop + "\".");
+      }
+    } while (hasSelected == false); //part of a do-while loop
+    System.out.println("You selected a(n) " + ChosenAnimal + ". Is this correct? (y/n)");
+    String UserInput = UserInputScanner.nextLine();
+  }
+}
+class BASE_Animals {
+  public String[] TypesOfAnimals = {"lion","tiger","wolf","rabbit","bear","turtle"};
+}
