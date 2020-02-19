9251541ea7149f010944c7ed983e49ebe22d441a
mwait101101 <59382740+mwait101101@users.noreply.github.com>
2020-02-19 16:48:47 -0500

    fixed a great deal of problems with the Animal Identification loop
    the program can now better parse user input and return the appropriate error messages when needed.

--- AnimalTypePicker.java
+++ AnimalTypePicker.java
@@ -3,7 +3,7 @@ public class AnimalTypePicker {
   static Scanner UserInputScanner = new Scanner(System.in);
   public static void main(String[] args) {
     BASE_Animals Animals = new BASE_Animals();
-    System.out.println("choose an animal from the following list (type the number to the right of the animal you would like to select):");
+    System.out.println("choose an animal from the following list (type the number to the right of the animal you would like to select OR type the the name of the animal you would like to select (case doesn't matter)):");
     for (int i=0; i<Animals.TypesOfAnimals.length; i++) {
       System.out.println("#" + (i+1) + ": " + Animals.TypesOfAnimals[i]);
       //------------
@@ -14,29 +14,30 @@ public class AnimalTypePicker {
     do {
       String UserInput = UserInputScanner.nextLine();
       try {
-        try {
         int ChosenAnimalByNumber = Integer.parseInt(UserInput);
+        if (ChosenAnimalByNumber < 0) {
+          System.out.println("The negative number you entered has been automatically converted to its positive counterpart.");
+        }
         AnimalWasChosenByItsNumber = true;
-        ChosenAnimal = Animals.TypesOfAnimals[ChosenAnimalByNumber-1];
+        ChosenAnimal = Animals.TypesOfAnimals[Math.abs(ChosenAnimalByNumber)-1];
         hasSelected = true;
-        }
-        catch (Exception AAA) {
-          System.out.println(AAA+"-------");
-          break;
-        }
       }
       catch (NumberFormatException ChosenAnimal_NFE) {
         AnimalWasChosenByItsNumber = false;
         for(int i=0; i<Animals.TypesOfAnimals.length; i++) {
-          if (ChosenAnimal.equalsIgnoreCase(Animals.TypesOfAnimals[i])) {
+          if (UserInput.equalsIgnoreCase(Animals.TypesOfAnimals[i])) {
             ChosenAnimal = Animals.TypesOfAnimals[i];
             hasSelected = true;
+            break;
           }
         }
+        if (hasSelected == false) {
+          System.out.println("Unfortunately, an error has occured and your input could not be resolved to an animal. Did you misspell the animal's name/number or input a special character? Try again!");
+        }
       }
-      catch (Exception EXCEPTION_AnimalIdentificationLoop) {
+/*      catch (Exception EXCEPTION_AnimalIdentificationLoop) {
         System.out.println("Unfortunately, an error of type \"" + EXCEPTION_AnimalIdentificationLoop + "\" has occurred in the \"AnimalIdentificationLoop\".");
-      }
+      }*/
     } while (hasSelected == false); //part of a do-while loop
     System.out.println("You selected a(n) " + ChosenAnimal + ". Is this correct? (y/n)");
     String UserInput = UserInputScanner.nextLine();
