fae676a758cde8df0ed095cb949ceca429c931cd
mwaitzman <51432220+mwaitzman@users.noreply.github.com>
2020-02-26 13:02:38 -0500

    finally figured out what was wrong with the creation of the new Animal object called lion and fixed it :)
    for the arrays in the call's parentheses, I just had the elements in square brackets, rather than their being prefaced by "new String[]" and enclosed in curly brackets.

--- Animal.java
+++ Animal.java
@@ -10,7 +10,7 @@ class Animal {
   final String[] Subspecies_TrinomialName;
   final String[] Genders;*/
 
-  Animal lion = new Animal("lion", "Animalia", "Chordata", "Mammalia", "Carnivora", "Feliformia", "Felidae", "Pantherinae", "Panthera", "P. leo", "Panthera leo", ["P. l. leo", "P. l. melanochaita", "P. l. sinhaleyus"], ["Panthera leo leo", "Panthera leo melanochaita", "Panthera leo sinhaleyus"], ["male", "female"]);
+  Animal lion = new Animal("lion", "Animalia", "Chordata", "Mammalia", "Carnivora", "Feliformia", "Felidae", "Pantherinae", "Panthera", "P. leo", "Panthera leo", new String[]{"P. l. leo", "P. l. melanochaita", "P. l. sinhaleyus"}, new String[]{"Panthera leo leo", "Panthera leo melanochaita", "Panthera leo sinhaleyus"}, new String[]{"male", "female"});
 
   ArrayList<Animal> ListOfAnimals = new ArrayList<>(1);
 
